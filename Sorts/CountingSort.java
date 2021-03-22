faa36ba7623d6e3d85c4e4f9462737f4304b2095
liuweibin <liuweibin3@sfmail.sf-express.com>
2021-03-22 14:16:08 +0800

    优化:理解优化后的计数排序

--- Sorts/CountingSort.java
+++ Sorts/CountingSort.java
@@ -61,7 +61,13 @@ class CountingSort implements SortAlgorithm {
                 .collect(toList());
     }
 
-    public static int[] countSort(int[] a) {
+    /**
+     * 未优化的计数排序
+     *
+     * @param a
+     * @return
+     */
+    public static int[] originCountSort(int[] a) {
         int b[] = new int[a.length];
         int max = a[0], min = a[0];
         for (int i : a) {
@@ -71,17 +77,69 @@ class CountingSort implements SortAlgorithm {
             if (i < min) {
                 min = i;
             }
-        }//这里k的大小是要排序的数组中，元素大小的极值差+1
-        int k = max - min + 1;
+        }
+        //这里k的大小是要排序的数组中，元素大小的极值差+1
+        int k = max + 1;
         int c[] = new int[k];
         for (int i = 0; i < a.length; ++i) {
-            c[a[i] - min] += 1;//优化过的地方，减小了数组c的大小
+            c[a[i]] += 1;
         }
+        //按存取的方式取出c的元素
+        int i = 0;
+        int count = 0;
+        while (i <= k - 1) {
+            for (int j = 0; j < c[i]; j++) {
+                b[count] = i;
+                count++;
+            }
+            i++;
+        }
+        return b;
+    }
+
+    /**
+     * 优化后的计数排序
+     *
+     * @param a
+     * @return
+     */
+    public static int[] countSort(int[] a) {
+        //使用最大值和最小值的方式是一种优化的计数排序
+        //可以兼容负数的情况，同时能减少存储的空间，比如最大数是100，但实际上只有90-100这10个数字
+        //所以仅仅需要10个存储空间即可
+        int max = a[0], min = a[0];
+        for (int i : a) {
+            max = Math.max(max, i);
+            min = Math.min(min, i);
+        }
+        System.out.println("max:" + max + "  min:" + min);
+        int k = max - min + 1;
+        System.out.println("count array len：" + k);
+
+        int c[] = new int[k];
+        //先是count计数词频
+        for (int i = 0; i < a.length; ++i) {
+            c[a[i] - min]++;
+        }
+        System.out.println("count: " + Arrays.toString(c));
+        // c[i]的值表示返回数组的位置
         for (int i = 1; i < c.length; ++i) {
             c[i] = c[i] + c[i - 1];
         }
-        for (int i = a.length - 1; i >= 0; --i) {
-            b[--c[a[i] - min]] = a[i];//按存取的方式取出c的元素
+
+        System.out.println("sumCount：" + Arrays.toString(c));
+
+        //存储最终的排序结果
+        int b[] = new int[a.length];
+        //这里从后向前遍历，如果从前往后的话，之后每个c[i]都要加1，代表位置往后移了一个位置
+        for (int i = a.length - 1; i >= 0; i--) {
+            int pos = a[i] - min;
+            int sumCount = c[pos];
+            System.out.println(a[i] + " 在原数组的排序后的位置是： " + (sumCount - 1));
+            b[sumCount - 1] = a[i];
+            // 位置减1
+            c[pos]--;
+
         }
         return b;
     }
@@ -102,6 +160,10 @@ class CountingSort implements SortAlgorithm {
         System.out.println("After Sorting By Streams:");
         print(streamSort(unsortedInts));
 
+        System.out.println("\n------------------------------\n");
+        for (int i : CountingSort.countSort(CountingSort.originCountSort(new int[]{4, 23, 6, 78, 1, 54, 23, 1, 9, 231, 9, 12}))) {
+            System.out.print(i + " ");
+        }
         System.out.println("\n------------------------------\n");
 
         // String Input
